96103933dace163ce16c42862b996dceaa7030b2
Russell Algeo <russellalgeo@gmail.com>
2026-03-14 13:45:40 -0400

    Add Vercel speed insights

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { App } from "./App";
 import "./styles.css";
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 );
